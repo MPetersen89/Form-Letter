a6f39bc50e2ddfd00a04a19a2cfce218c349e54a
Michael <michael_petersen_email@yahoo.com>
2020-06-03 10:30:43 -0700

    update form letter to include more inputs/interactivity.
    Co-authored by: Kevin Davis <thekidnamedkd@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,12 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     const name = $("input#name").val();
+    const title = $("input#title").val();
+    const address = $("input#address").val();
 
     $(".name").text(name);
+    $(".title").text(title);
+    $(".address").text(address);
 
     $("#story").show();
 
